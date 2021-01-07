2b0fbef8211b6138f13d98110bd136f367847e15
kolya-kra <kolya.krafeld@gmx.de>
2021-01-07 22:26:23 +0100

    Added styles to start page

--- src/views/Start.jsx
+++ src/views/Start.jsx
@@ -6,8 +6,40 @@ import {
 } from '../lib/authorization';
 
 import { RoundButton as Button } from '../components/RoundButton';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, withTheme } from '@material-ui/core/styles';
 
-const Start = () => {
+const useStyles = makeStyles((theme) => ({
+  titleContainer: {
+    position: 'absolute',
+    top: '10%',
+    left: '50%',
+    transform: 'translateX(-50%)',
+  },
+  title: {
+    marginTop: -20,
+    marginBottom: -20,
+    fontSize: '48pt',
+    textAlign: 'left',
+  },
+  description: {
+    margin: 'auto',
+    textAlign: 'center',
+    margin: '22rem 1rem 0rem 1rem',
+  },
+  descriptionText: {
+    textAlign: 'left',
+    display: 'inline-block',
+  },
+  button: {
+    marginTop: '5rem',
+  },
+}));
+
+const Start = (props) => {
+  const { theme } = props;
+  const classes = useStyles();
+
   const authorizeUser = async () => {
     const verifier = await generateVerifierString();
     localStorage.setItem('verifier', verifier);
@@ -32,13 +64,31 @@ const Start = () => {
 
   return (
     <div>
-      <h1>Festival Playlist Generator</h1>
+      <div className={classes.titleContainer}>
+        <h1 className={classes.title}>Festival</h1>
+        <h1 className={classes.title}>Playlist</h1>
+        <h1 className={classes.title}>Generator</h1>
+      </div>
 
-      <Button onClick={authorizeUser} variant="contained" color="primary">
+      <div className={classes.description}>
+        <Typography variant="subtitle1" className={classes.descriptionText}>
+          Create a Spotify Playlist for your next music festival!
+        </Typography>
+        <Typography variant="body2" className={classes.descriptionText}>
+          Simply upload a line-up image or type in the upcoming acts.
+        </Typography>
+      </div>
+
+      <Button
+        onClick={authorizeUser}
+        variant="contained"
+        color="primary"
+        className={classes.button}
+      >
         Create Playlist
       </Button>
     </div>
   );
 };
 
-export default Start;
+export default withTheme(Start);
