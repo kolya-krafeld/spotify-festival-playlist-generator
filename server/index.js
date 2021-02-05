85bd802476549c99dd4d595659c58d1051aad64f
kolya-kra <kolya.krafeld@gmx.de>
2021-02-05 16:02:29 +0100

    Added /api/ocr endpoint

--- server/index.js
+++ server/index.js
@@ -1,24 +1,32 @@
 const express = require('express');
+const { checkError } = require('./multerLogic');
+const vision = require('@google-cloud/vision');
+
+const client = new vision.ImageAnnotatorClient({
+  keyFilename: 'GoogleVisionKey.json',
+});
 
 const app = express();
 
-quickstart();
+app.post('/api/ocr', async (req, res) => {
+  try {
+    const imageDesc = await checkError(req, res);
+
+    //Use the mv() method to place the file in upload directory (i.e. "uploads")
+    let ocrText = await visionOcr(imageDesc);
+
+    //send response
+    res.send(ocrText);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server Startet'));
 
-async function quickstart() {
-  // Imports the Google Cloud client library
-  const vision = require('@google-cloud/vision');
-
-  // Creates a client
-  const client = new vision.ImageAnnotatorClient({
-    keyFilename: 'GoogleVisionKey.json',
-  });
-
-  // Performs label detection on the image file
-  const [result] = await client.textDetection('./rar.jpg');
-  console.log(result.fullTextAnnotation.text);
-  const detections = result.textAnnotations;
+async function visionOcr(img) {
+  const [result] = await client.textDetection(img.path);
+  return result.fullTextAnnotation.text;
 }
